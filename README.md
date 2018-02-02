63388731c15de0a06ea61624ae00886cd6ef13a6
Donovan Preston <donovanpreston@gmail.com>
2018-02-02 11:05:30 -0500

    Switch to a WebExtension and WebExtension API Experiment

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
+
 Tab Split
 =========
 
 Tab Split is a Firefox Test Pilot experiment which allows you to see two tabs in the same window, side by side.
 
-Currently, Tab Split is a legacy bootstrap extension. In the future, the implementation will be changed to be a webextension with an embedded Web Extension API Experiment.
+Currently, Tab Split uses a WebExtension API Experiment. Currently there is a [bug](https://bugzilla.mozilla.org/show_bug.cgi?id=1434076) which prevents us from using Embedded WebExtension API Experiments, so installation is a two step process.
 
-Running
--------
-Tab Split requires Node 8 and Firefox Nightly.
+Start Firefox Nightly, visit `about:debugging`, click `Load Temporary Add-on`, and navigate to `tab-split/experiment/schema.json`. Then click `Load Temporary Add-on` again, and navigate to `tab-split/webext/manifest.json`.
 
-Running `npm install && npm run addon` inside the tab-split directory will start a Firefox Nightly instance with tab split loaded. Click on the tab split icon in the toolbar to try it out.
---
+++ experiment/api.js
@@ -0,0 +1,85 @@
+
+const { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+
+
+class Impl {
+  constructor() {
+    this._browserCount = 0;
+  }
+
+  async init(context) {
+    console.log("TMP > TabSplit - api - onNewBrowserCreated");
+
+    let WM = Cc['@mozilla.org/appshell/window-mediator;1'].getService(Ci.nsIWindowMediator);
+    let chromeWindow = WM.getMostRecentWindow("navigator:browser");
+    let tabbrowser = chromeWindow.document.getElementById("content");
+    if (tabbrowser.getAttribute("data-tabsplit-tabbrowser-id")) {
+      return chromeWindow;
+    }
+
+    if (this._browserCount === 0) {
+      console.log("TMP > TabSplit - api - onNewBrowserCreated context.callOnClose");
+      context.callOnClose({
+        close: () => {
+          console.log("TMP > TabSplit - api - close");
+          this.destroy();
+        }
+      });
+    }
+
+    tabbrowser.setAttribute("data-tabsplit-tabbrowser-id", ++this._browserCount);
+    console.log("TMP > TabSplit - api - onNewBrowserCreated - browserCount", this._browserCount);
+    console.log("TMP > TabSplit - api - onNewBrowserCreated - load overlay tabsplit-init-overlay.xul");
+    chromeWindow.document.loadOverlay("resource://extension-tabsplit-api/chrome/overlay/tabsplit-init-overlay.xul",
+      (subj, topic, data) => console.log("TMP > TabSplit - api - onNewBrowserCreated - load overlay topic", topic));
+
+    return chromeWindow;
+  }
+
+  async tabsplit(_context) {
+    console.log("TMP > TabSplit - api - tabsplit");
+
+    const win = await this.init();
+    await win.TabSplit.view.clickCallback();
+
+    return true;
+  }
+
+  async destroy() {
+    console.log("TMP > TabSplit - api - onDestroy");
+    let WM = Cc['@mozilla.org/appshell/window-mediator;1'].getService(Ci.nsIWindowMediator);
+    let chromeWindows = WM.getEnumerator("navigator:browser");
+    while (chromeWindows.hasMoreElements()) {
+      let win = chromeWindows.getNext();
+      let tabbrowser = win.document.getElementById("content");
+      if (win.TabSplit) {
+        console.log("TMP > TabSplit - api - destroying data-tabsplit-tabbrowser-id =", tabbrowser.getAttribute("data-tabsplit-tabbrowser-id"), Date.now());
+        win.TabSplit.control.destroy();
+        delete win.TabSplit;
+        console.log("TMP > TabSplit - api - destroyed at", Date.now());
+      }
+      tabbrowser.removeAttribute("data-tabsplit-tabbrowser-id");
+      this._browserCount--;
+    }
+    return true;
+  }
+}
+
+
+class API extends ExtensionAPI {
+  getAPI(context) {
+    const impl = new Impl();
+    return {
+      tabsplit: {
+        init: async () => {
+          await impl.init(context);
+          return "init";
+        },
+        tabsplit: async () => {
+          await impl.tabsplit(context);
+          return "tabsplit";
+        }
+      }
+    }
+  }
+}
